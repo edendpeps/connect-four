12462668487aa4e833d07f6a45dcb78e0e41e8a7
edendpeps <eden080720@naver.com>
2025-10-22 13:52:30 +0900

    커넥트포 완성 미니맥스부터 구현

--- mini-max/mini-max.cpp
+++ mini-max/mini-max.cpp
@@ -1,4 +1,21 @@
-﻿
+﻿#include <string>
+#include <array>
+#include <vector>
+#include <sstream>
+#include <utility>
+#include <random>
+#include <assert.h>
+#include <math.h>
+#include <chrono>
+#include <algorithm>
+#include <iostream>
+#include <functional>
+#include <queue>
+#include <set>
+
+using ScoreType = int64_t;
+
+
 int main()
 {
 
